2d1bbf8d5613661a678eabc8a17c2f27af7dd908
AbhishekChapagai <57378876+AbhishekChapagai@users.noreply.github.com>
2021-07-26 20:57:48 +0545

    Landing updated

--- src/components/Pages/LandingPage/landing.js
+++ src/components/Pages/LandingPage/landing.js
@@ -7,29 +7,6 @@ import './landing.css';
 
 class landing extends Component {
 
-    state = {
-        cosmetic: [],
-        gadget: [],
-        config: {
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        },
-    }
-
-    componentDidMount() {
-        axios.get("http://localhost:90/cosmetic/five", this.state)
-            .then((response) => {
-                console.log(response)
-                this.setState({
-                    cosmetic: response.data.data
-                })
-            })
-            .catch((err) => {
-                console.log(err.response)
-            })
-    }
-
     render() {
 
         return (
@@ -46,65 +23,6 @@ class landing extends Component {
                         <LandingP>COSMETICS | LAPTOPS | CAMERAS</LandingP>
                     </LandingContent>
                 </LandingContainer>
-
-                {/* COSMETIC CATEGORY */}
-                <div className="displayCosmetic">
-                    <div className="showCosmetic">
-                        <div className="cosmeticBand">
-                            <p>Cosmetics</p>
-                            <div className="viewMore">View more </div>
-                        </div>
-                        {
-                            this.state.cosmetic.map((c) => {
-                                return (
-                                    <div className="cosmetic">
-                                        <p><img src={"http://localhost:90/assets/image/cosmetic/" + c.cosmeticimage} alt="img" /></p>
-                                        <p className="CosmeticName">Name: &nbsp;
-                                            {
-                                                c.cosmeticname
-                                            }
-                                        </p>
-                                        <p>
-                                            {/* <Link to={"/vehicleDetails/" + product._id}> <button className="btnView" >DETAILS</button></Link> */}
-                                        </p>
-                                    </div>
-                                )
-                            })
-                        }
-
-                    </div>
-                </div>
-
-                {/* GADGET CATEGORY */}
-
-                <div class="displayGadget">
-                    <div class="showGadget">
-                        <div className="gadgetBand">
-                            <p>Gadgets</p>
-                            <div className="viewMore">View more </div>
-                        </div>
-                        {
-                            this.state.cosmetic.map((g) => {
-                                return (
-                                    <div class="product">
-                                        <p><img src={"http://localhost:90/assets/image/cosmetic/" + g.gadgetimage} alt="img" /></p>
-                                        <p>Name: &nbsp;
-                                            {
-                                                g.gadgetname
-                                            }
-                                        </p>
-                                        <p>
-                                            {/* <Link to={"/vehicleDetails/" + product._id}> <button className="btnView" >DETAILS</button></Link> */}
-                                        </p>
-                                    </div>
-                                )
-                            })
-                        }
-
-                    </div>
-                </div>
-
-
 
             </>
         )
