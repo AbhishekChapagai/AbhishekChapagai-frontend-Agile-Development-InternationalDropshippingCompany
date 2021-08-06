c8e42aff660bcc94b96317bc506dd0fe1a8dea98
BikashJhendi <bikashjhendi11@gmail.com>
2021-08-06 17:11:14 +0545

    password show/hide added into login and register

---
+++ src/components/Pages/LoginRegister/VerifyEmail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class VerifyEmail extends Component {
+    render() {
+        return (
+            <div className="VerifyEmail">
+                <span> Hello first name, We have send you an email. Please verify your email address in order to enjoy our services.
+                </span>
+            </div>
+        )
+    }
+}
+
+export default VerifyEmail;
