9b329415b6ef86d018c873c8734d17940242b8c7
BikashJhendi <bikashjhendi11@gmail.com>
2021-09-01 19:53:15 +0545

    adduser complete

--- src/components/Pages/AdminDashboard/User/AddUser.js
+++ src/components/Pages/AdminDashboard/User/AddUser.js
@@ -1,5 +1,9 @@
+import axios from 'axios';
 import React, { Component } from 'react'
+import { toast } from 'react-toastify';
 import { districts, provinces } from '../../List/AddressList';
+import { userTypes } from '../../List/StatusList';
+toast.configure();
 
 export default class AddUser extends Component {
     state = {
@@ -7,10 +11,13 @@ export default class AddUser extends Component {
         lastname: "",
         email: "",
         password: "",
+        phone: "",
         province: "",
         district: "",
-        provinceSelected: "",
-        districtsOption: [],
+        address: "",
+        tole: "",
+        zipCode: "",
+        userType: "",
         passwordHidden: true,
         config: {
             headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
@@ -28,9 +35,47 @@ export default class AddUser extends Component {
         });
     }
 
+    submitData = (e) => {
+        e.preventDefault();
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/admin/user/add`, this.state, this.state.config)
+            .then((response) => {
+                console.log(response.data.success);
+                this.setState({
+                    success: response.data.success
+                })
+
+                toast.success('User added.', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            })
+            .catch((err) => {
+                console.log(err.response);
+
+                this.setState({
+                    success: err.response.data.success
+                })
+
+                toast.error('Failed to add user!!!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+            })
+    }
+
     render() {
         return (
-            <div className="AddUser" >
+            <div className="AddUser mb-3" >
                 <div className="container req_left_container">
                     {/* form */}
                     <div className="req_border_container col-12 col-sm-12 col-md-9 col-lg-6">
@@ -46,28 +91,26 @@ export default class AddUser extends Component {
                                 </div>
 
                                 <div className="row g-2">
-                                    <div className="col-md-6">
+                                    <div className="col-sm-6">
                                         <div className="form-floating mb-2">
                                             <input type="text" className="form-control" id="floatingInputFirst" placeholder="First Name" name="firstname"
-                                                data-testid="" onChange={this.changeHandler} />
+                                                value={this.state.firstname} data-testid="" onChange={this.changeHandler} />
                                             <label id="firstname" htmlFor="floatingInputFirst">First Name*</label>
                                         </div>
                                     </div>
 
-                                    <div className="col-md-6">
+                                    <div className="col-sm-6">
                                         <div className="form-floating mb-2">
                                             <input type="text" className="form-control" id="floatingInputLast" placeholder="Last Name" name="lastname"
-                                                data-testid="" onChange={this.changeHandler} />
+                                                value={this.state.lastname} data-testid="" onChange={this.changeHandler} />
                                             <label id="lastname" htmlFor="floatingInputLast">Last Name*</label>
                                         </div>
                                     </div>
                                 </div>
 
-
-
                                 <div className="form-floating mb-2">
                                     <input type="email" className="form-control" id="floatingInputEmail" placeholder="name@example.com" name="email"
-                                        data-testid="email-input" value={this.state.email} onChange={this.changeHandler} />
+                                        value={this.state.email} data-testid="email-input" value={this.state.email} onChange={this.changeHandler} />
                                     <label id="email" htmlFor="floatingInputEmail">Email address*</label>
                                     {this.state.email && !(/\S+@\S+\.\S+/).test(this.state.email) && <span className="error" data-testid="error-msg">Please enter a valid email.</span>}
 
@@ -75,7 +118,7 @@ export default class AddUser extends Component {
 
                                 <div className="form-floating mb-2 input_right_icon">
                                     <input type={this.state.passwordHidden ? 'password' : 'text'} className="form-control form_control_input" id="floatingPassword" placeholder="Password" name="password"
-                                        data-testid="password-input" value={this.state.password} onChange={this.changeHandler} />
+                                        value={this.state.password} data-testid="password-input" value={this.state.password} onChange={this.changeHandler} />
                                     <i id="input_form_right_icon" className="form_right_icon"
                                         onClick={this.toggleShow}>
                                         {this.state.passwordHidden ? < i className="fas fa-eye-slash icon_change"></i> : < i className="fas fa-eye icon_change"></i>}
@@ -85,8 +128,8 @@ export default class AddUser extends Component {
 
                                 <div className="col-md-12">
                                     <div className="form-floating mb-2">
-                                        <input type="text" className="form-control" id="floatingInputPhone" placeholder="Phone Name" name="phone"
-                                            data-testid="" onChange={this.changeHandler} />
+                                        <input type="number" className="form-control" id="floatingInputPhone" placeholder="Phone Name" name="phone"
+                                            value={this.state.phone} data-testid="" onChange={this.changeHandler} />
                                         <label id="phone" htmlFor="floatingInputPhone">Phone Number</label>
                                     </div>
                                 </div>
@@ -97,7 +140,7 @@ export default class AddUser extends Component {
                                 </div>
 
                                 <div className="row g-2">
-                                    <div className="form-floating mb-2 col-6">
+                                    <div className="form-floating mb-2 col-12 col-sm-6">
                                         <select className="form-select" id="floatingProvince" aria-label="Floating label select example"
                                             value={this.state.province}
                                             name="province" onChange={this.changeHandler}>
@@ -107,15 +150,16 @@ export default class AddUser extends Component {
                                                     {option.label}
                                                 </option>)
                                             }
-                                            {/* <option selected>Select product type</option>
-                                        <option>Gadget</option>
-                                        <option>Cosmetic</option>
-                                        <option>Other</option> */}
+                                            {/* 
+                                            <option selected>Select product type</option>
+                                            <option>Gadget</option>
+                                            <option>Cosmetic</option>
+                                            <option>Other</option> */}
                                         </select>
                                         <label htmlFor="floatingProvince">Province</label>
                                     </div>
 
-                                    <div className="form-floating mb-2 col-6">
+                                    <div className="form-floating mb-2 col-12 col-sm-6">
                                         <select className="form-select" id="floatingDistrict" aria-label="Floating label select example"
                                             value={this.state.district}
                                             name="district" onChange={this.changeHandler}>
@@ -135,14 +179,14 @@ export default class AddUser extends Component {
                                     <div className="col-md-8">
                                         <div className="form-floating mb-2">
                                             <input type="text" className="form-control" id="floatingAddress" placeholder="Address" name="address"
-                                                data-testid="" onChange={this.changeHandler} />
+                                                value={this.state.address} data-testid="" onChange={this.changeHandler} />
                                             <label id="address" htmlFor="floatingAddress">Address</label>
                                         </div>
                                     </div>
                                     <div className="col-md-4">
                                         <div className="form-floating mb-2">
                                             <input type="text" className="form-control" id="floatingTole" placeholder="Tole" name="tole"
-                                                data-testid="" onChange={this.changeHandler} />
+                                                value={this.state.tole} data-testid="" onChange={this.changeHandler} />
                                             <label id="tole" htmlFor="floatingTole">Tole</label>
                                         </div>
                                     </div>
@@ -151,21 +195,37 @@ export default class AddUser extends Component {
                                 <div className="col-md-12">
                                     <div className="form-floating mb-2">
                                         <input type="number" className="form-control" id="floatingZipCode" placeholder="Zip Code" name="zipCode"
-                                            data-testid="" onChange={this.changeHandler} />
+                                            value={this.state.zipCode} data-testid="" onChange={this.changeHandler} maxLength="2" min="10" max="200" />
                                         <label id="zipCode" htmlFor="floatingZipCode">Zip Code</label>
                                     </div>
                                 </div>
 
                                 <div className="form_heading mt-3">
-                                    <h2>Address</h2>
+                                    <h2>Account Type</h2>
                                     <hr />
                                 </div>
 
+                                <div className="form-floating mb-2 col-12">
+                                    <select className="form-select" id="floatingUserType" aria-label="Floating label select example"
+                                        value={this.state.userType}
+                                        name="userType" onChange={this.changeHandler}>
+
+                                        {userTypes.map(option =>
+                                            <option key={option.label} value={option.value}>
+                                                {option.label}
+                                            </option>)
+                                        }
+                                    </select>
+                                    <label htmlFor="floatingUserType">User Type*</label>
+                                </div>
+
+                                <span className="p-1 ">* = Required Field.</span>
                                 <button type="submit" id="btn_req_product" className="btn btn_primary_color btn-md btn-block"
                                     onClick={this.submitData}>Request Product</button>
                             </form>
 
-                            {/* <p className="r_form_agree">
+                            {/* 
+                            <p className="r_form_agree">
                                         By clicking Register, you agree to our <b>Terms of Service </b> and <b>Privacy Policy</b>.
                                     </p>
 
---
+++ src/components/Pages/List/StatusList.js
@@ -0,0 +1,18 @@
+const userTypes = [
+    {
+        id: "0",
+        label: "Select User Type",
+    },
+    {
+        id: "1",
+        label: "User",
+        value: 'User',
+    },
+    {
+        id: "2",
+        label: "Admin",
+        value: 'Admin',
+    },
+]
+
+export { userTypes }
